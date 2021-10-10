9b05349ce08a8830c289b8bb036d98e773174482
DESKTOP-Q313TGL\rapha <raphaelguizan@gmail.com>
2021-10-10 02:53:33 -0300

    feat: velho com animação e ragindo a colisão com a galinha

--- Assets/_Project/Scripts/Farmer/FarmerController.cs
+++ Assets/_Project/Scripts/Farmer/FarmerController.cs
@@ -6,15 +6,20 @@ using UnityEngine.AI;
 public class FarmerController : MonoBehaviour
 {
     public Transform farmerPoints;
-    public float speed = 2;
     public NavMeshAgent agent;
+    public Animator anim;
+    [Space]
+    public float chickenCooldown = 5f;
 
+    private bool canBeHited = true;
+    private float initalSpeed;
     private List<Transform> points;
     private int pathIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
         points = new List<Transform>();
+        initalSpeed = agent.speed;
         for (int i = 0; i < farmerPoints.childCount; i++)
         {
             points.Add(farmerPoints.GetChild(i));
@@ -45,5 +50,33 @@ public class FarmerController : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void MoveToggle(string b)
+    {
+        if (b.Equals("walk"))
+        {
+            agent.speed = initalSpeed;
+        }
+        else
+        {
+            agent.speed = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && canBeHited)
+        {
+            canBeHited = false;
+            anim.SetTrigger("angry");
+            StartCoroutine(ChickenCoolDownCount());
+        }
+    }
+
+    IEnumerator ChickenCoolDownCount()
+    {
+        yield return new WaitForSeconds(chickenCooldown);
+        canBeHited = true;
     }
 }
