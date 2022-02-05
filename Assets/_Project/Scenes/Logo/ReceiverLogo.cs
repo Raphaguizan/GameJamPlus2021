b191e9f11d844bb02416e10a9afeb6b743adf541
DESKTOP-Q313TGL\rapha <raphaelguizan@gmail.com>
2022-02-04 23:22:20 -0300

    cena de logo

---
+++ Assets/_Project/Scenes/Logo/ReceiverLogo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiverLogo : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        GameManager.Instance.MainMenu(true, 0);
+    }
+}
