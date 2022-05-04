9a81ccaab0fbaf674b57b665eb632328b1fc0521
Raphaguizan <raphaelguizan@gmail.com>
2022-05-03 21:34:08 -0300

    Colisão da karen com as galinhas estudantes

--- Assets/Scripts/FollowTheChick/StudentChicken/StudentChicken.cs
+++ Assets/Scripts/FollowTheChick/StudentChicken/StudentChicken.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Game.Chicken;
 
 namespace Game.Minigame.FollowTheChick
 {
@@ -21,6 +22,7 @@ namespace Game.Minigame.FollowTheChick
             if(animator == null)animator = GetComponent<Animator>();
         }
 
+        #region Chick
         public StudentChicken SelectChicken()
         {
             navMeshObstacle.enabled = false;
@@ -32,11 +34,29 @@ namespace Game.Minigame.FollowTheChick
             if (animator) animator.SetTrigger(animationString);
             StartCoroutine(NavMeshDelay(true, timeDelay + 1f));
         }
-
+        
         IEnumerator NavMeshDelay(bool val, float time = 0)
         {
             yield return new WaitForSeconds(time);
             navMeshObstacle.enabled = val;
         }
+        #endregion
+
+        #region Karen Interactions
+
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.name);
+            if (other.gameObject.GetComponent<Chicken.Chicken>())
+            {
+                MakeAnimation();
+            }
+        }
+
+        public void MakeAnimation()
+        {
+            if (animator) animator.SetTrigger(animationString);
+        }
+        #endregion
     }
 }
