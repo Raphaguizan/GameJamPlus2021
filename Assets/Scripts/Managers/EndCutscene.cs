336030305600c94db999985f77e51b4234988aa4
brunopaivasantos <48925394+brunopaivasantos@users.noreply.github.com>
2022-05-30 15:50:51 -0300

    Finais desenhados

--- Assets/Scripts/Managers/EndCutscene.cs
+++ Assets/Scripts/Managers/EndCutscene.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class EndCutscene : MonoBehaviour
 {
+    [SerializeField] PlayableDirector cutscene;
+    [SerializeField] PlayableDirector final;
+    [SerializeField] bool isFinal;
    public void End()
     {
         GameManager.Instance.MainMenu(true, 1);
@@ -14,4 +18,19 @@ public class EndCutscene : MonoBehaviour
         GameManager.Instance.StartGame();
     }
 
+    public void CheckCutscene()
+    {
+        if(isFinal)
+        {
+            cutscene.Pause();
+            StartCoroutine(StopCutscene());
+            final.Play();
+        }
+    }
+
+    IEnumerator StopCutscene()
+    {
+        yield return new WaitForSeconds(2f);
+        cutscene.Stop();
+    }
 }
